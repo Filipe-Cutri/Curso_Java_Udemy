b008105877ffc95bae695915dce0bffe52eb5526
Filipe <filipe.cutri18@gmail.com>
2025-02-05 23:59:12 -0300

    Ex-01_Iniciantes

---
+++ EstruturaSequencial/src/ExerciciosIniciantes/Ex_01.java
@@ -0,0 +1,17 @@
+package ExerciciosIniciantes;
+
+import java.util.Scanner;
+
+public class Ex_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int n1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int n2 = scanner.nextInt();
+
+        System.out.println("Soma = " + n1 + n2);
+    }
+}
