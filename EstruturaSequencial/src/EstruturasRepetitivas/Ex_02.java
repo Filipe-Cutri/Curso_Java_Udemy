70001925eb1bb030745ef8c523e61d07256dd016
Filipe <filipe.cutri18@gmail.com>
2025-02-15 18:30:31 -0300

    Ex-02: Loop While

---
+++ EstruturaSequencial/src/EstruturasRepetitivas/Ex_02.java
@@ -0,0 +1,49 @@
+package EstruturasRepetitivas;
+
+import java.util.Scanner;
+
+public class Ex_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        int opcao;
+
+        System.out.println("Escolha o tipo de combustível:");
+        System.out.println("1 - Álcool");
+        System.out.println("2 - Gasolina");
+        System.out.println("3 - Diesel");
+        System.out.println("4 - Fim");
+
+        while (true) {
+            opcao = sc.nextInt();
+
+            if (opcao == 4) {
+                break;
+            }
+
+            switch (opcao) {
+                case 1:
+                    alcool++;
+                    break;
+                case 2:
+                    gasolina++;
+                    break;
+                case 3:
+                    diesel++;
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
+
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Álcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
+    }
+}
