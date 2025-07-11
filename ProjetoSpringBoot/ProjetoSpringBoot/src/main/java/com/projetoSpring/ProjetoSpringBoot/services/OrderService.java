bf77c0dbbe50b542e1ee1e5de323eb69a82e145b
Filipe <filipe.cutri18@gmail.com>
2025-07-10 22:49:08 -0300

    Criando projeto com SpringBoot e Maven

---
+++ ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/com/projetoSpring/ProjetoSpringBoot/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.projetoSpring.ProjetoSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoSpring.ProjetoSpringBoot.entities.Order;
+import com.projetoSpring.ProjetoSpringBoot.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
---
+++ ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/com/projetoSpring/ProjetoSpringBoot/services/UserService.java
@@ -0,0 +1,26 @@
+package com.projetoSpring.ProjetoSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoSpring.ProjetoSpringBoot.entities.User;
+import com.projetoSpring.ProjetoSpringBoot.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
