65b17de656889aeea0e733387ebb29d82d762bc3
Filipe <filipe.cutri18@gmail.com>
2025-07-16 00:40:06 -0300

    Consulta com vários filtros

--- workshopmongo/src/main/java/com/example/demo/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Post;
@@ -8,4 +12,12 @@ import com.example.demo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post>searchTittle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: { $gte: ?1 } }, { $or: [ { title: { $regex: ?0, $options: 'i' } }, { body: { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] }, {  date: { $lte: ?2 } } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
 }
--- workshopmongo/src/main/java/com/example/demo/services/PostService.java
+++ workshopmongo/src/main/java/com/example/demo/services/PostService.java
@@ -1,5 +1,8 @@
 package com.example.demo.services;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,15 @@ public class PostService {
 	public Post findById(String id) {
 	    return repo.findById(id)
 	               .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. ID: " + id));
+	}
+	
+	public List<Post> findByTittle(String text) {
+		return repo.searchTittle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
